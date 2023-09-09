82be397a5a0f348c090b267b6ae0520bf2cbb001
Kari Burt <kandreahburt@gmail.com>
2023-09-09 12:31:12 -0400

    commenting out tests

--- src/test/java/com/myke/demo/repository/MykeRepositoryTest.java
+++ src/test/java/com/myke/demo/repository/MykeRepositoryTest.java
@@ -1,37 +1,37 @@
-package com.myke.demo.repository;
+// package com.myke.demo.repository;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import java.sql.Time;
+// import java.text.DateFormat;
+// import java.time.Instant;
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.time.LocalTime;
+// import java.time.format.DateTimeFormatter;
+// import java.util.Date;
 
-import com.myke.demo.model.Myke;
+// import com.myke.demo.model.Myke;
 
-public class MykeRepositoryTest {
+// public class MykeRepositoryTest {
 
-    @Autowired
-    private MykeRepository mykeRepository; 
+//     @Autowired
+//     private MykeRepository mykeRepository; 
 
 
-    @Test
-    void isShouldFindAll(){
-        //given
-        LocalTime start_time = LocalTime.parse("14:00:00");  
-        String oldstring = "2023-08-03 00:00:00.0";
-        LocalDate datetime = LocalDate.parse(oldstring, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S")); 
-        Myke myke1 = new Myke(1, "YKA", 1, start_time, "Tuesday", "Manhattan", 1, 1, 0, datetime); 
+//     @Test
+//     void isShouldFindAll(){
+//         //given
+//         LocalTime start_time = LocalTime.parse("14:00:00");  
+//         String oldstring = "2023-08-03 00:00:00.0";
+//         LocalDate datetime = LocalDate.parse(oldstring, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S")); 
+//         Myke myke1 = new Myke(1, "YKA", 1, start_time, "Tuesday", "Manhattan", 1, 1, 0, datetime); 
 
 
         
-        //when
-        //then 
+//         //when
+//         //then 
         
-    }
+//     }
      
-}
+// }
